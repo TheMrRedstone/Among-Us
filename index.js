af50ee95f269808637ff909440709c860fffea69
TheMrRedstone <87324700+TheMrRedstone@users.noreply.github.com>
2021-12-10 14:52:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-window.addEventListener('load', function () {
-    var speed = 10,
-    direction = 1;
-    var boxElement = document.getElementById('Player');
+var Player = document.getElementById("Player")
 
-    if (boxElement) {
-        boxElement.addEventListener('mouseover', function () {
-            var boxLeftPos = boxElement.offsetLeft,
-                boxRightPos = boxLeftPos + boxElement.offsetWidth;
-            if (boxRightPos > document.body.offsetWidth) {
-                direction = -1;
-            }
-            // When left side of the box goes too far - change direction:
-            if (boxLeftPos < 0) {
-                direction = 1;
-            }
-            boxElement.style.left = (boxLeftPos + speed * direction) + 'px';
-        });
-    }
-});
+function movePlayerLeft() {
+  var leftNumbers = Player.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
 
-    window.addEventListener('keydown', (event) => {
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+window.addEventListener('keydown', (event) => {
       if (event.key == 'w') {
-        event.preventDefault();
-        console.log(event)
-        console.log("You Pressed W")
+        movePlayerLeft()
       }
 })
